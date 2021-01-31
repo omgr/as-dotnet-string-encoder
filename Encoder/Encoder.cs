b8aefa2e761611203922d2583fe64579ffdb84f3
V S S S Madan Gopal Rao Ongole <ongolemadangopal@gmail.com>
2021-01-31 20:12:46 +0530

    [Madan Gopal Rao Solution] Updated the Encoder.cs with solution to encode given string based on rules provided in problem statement.

--- Encoder/Encoder.cs
+++ Encoder/Encoder.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Encoder
 {
@@ -6,8 +8,59 @@ namespace Encoder
     {
         public string Encode(string message)
         {
-            //Implement your code here!
-            throw new NotImplementedException();
+
+            message = message.ToLower(); //making string case insensitive.
+            string encodedString = ""; //resultant encoded string is populated here
+            Dictionary<char, string> vowelAndSpaceRules = new Dictionary<char, string>
+                                                                { { 'a', "1" },
+                                                                  { 'e', "2" },
+                                                                  { 'i', "3" },
+                                                                  { 'o', "4" },
+                                                                  { 'u', "5" },
+                                                                  { 'y', " " },
+                                                                  { ' ', "y" }
+                                                                };
+            for (int i = 0; i < message.Length; i++)
+            {
+
+                if (vowelAndSpaceRules.Keys.Contains(message[i]))
+                {
+                    //replaces all the vowels, spaces and charecter 'y' with encoding charecters.
+                    encodedString = encodedString + vowelAndSpaceRules[message[i]];
+                }
+                else if (char.IsDigit(message[i]))
+                {
+                    //once a digit is encountered, tracks all the digits and revers them as part of encoding.
+                    string num = "";
+                    while (i < message.Length && char.IsDigit(message[i]))
+                    {
+                        num = num + message[i].ToString();
+                        i++;
+                    }
+                    encodedString = encodedString + String.Join("", num.Reverse().ToArray());
+                    i--;
+                }
+                else
+                {
+                    //finally, for rest of the charecters, 
+                    //if it is a alphabet, it's previous letter would be pushed to encoded string 
+                    //otherwise same charecter is pushed.
+                    if (char.IsLetter(message[i]))
+                    {
+                        int ascii = (int)Convert.ToChar(message[i]);
+                        ascii = ascii - 1;
+                        char encodedChar = (char)ascii;
+                        encodedString = encodedString + encodedChar;
+
+                    }
+                    else
+                    {
+                        encodedString = encodedString + message[i];
+                    }
+                }
+
+            }
+            return encodedString;
         }
     }
 }
